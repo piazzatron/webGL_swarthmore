b213786dc9b5a95311d562173775aeb1c6c41064
Michael Piazza <michaelpiazza@dhcp-130-58-101-57.swarthmore.edu>
2017-02-20 19:29:58 -0500

    Refactor working

--- camera/index.js
+++ camera/index.js
@@ -1,335 +1,305 @@
-"use strict";
+/*jshint esversion: 6 */
 
-var NUM_TRIANGLES = 1;
-var FOV = 45;
-var NEAR = 0.01;
-var FAR = 10;
-var OBJ_TRANSLATION = [0, 0, -3.5];
-var DRAW_NORMALS = true;
-var ROTATION_SPEED = 0.0;
-var CAMERA_TRANSLATION = [0, 0, 0];
-var CAMERA_LOOK = [0, 0, -1];
-var PAUSE = false;
-var CLEAR_COLOR = [0.9, 0.6, 0.0, 1.0];
+  'use strict';
 
-var gl;
-var canvas;
-var vertexBuffer;
-var colorBuffer;
-var normalBuffer;
-var normalLineBuffer;
-var lights;
-var program;
-var positions;
-var normals;
-var normalLines;
-var camera;
-var mvMatrix = mat4.create();
-var pMatrix = mat4.create();
-var tMatrix = mat4.create();
-var cameraMatrix = mat4.create();
-var tInv = mat4.create();
-var previousLoc = {
-	x: 300,
-	y: 250
-};
+  const NUM_SHAPES = 1;
+  const FOV = 45;
+  const NEAR = 0.01;
+  const FAR = 10;
+  let DRAW_NORMALS = true;
+  let ROTATION_SPEED = 0.0;
+  let CAMERA_TRANSLATION = [0, 0, 1.5];
+  let CAMERA_LOOK = [0, 0, 0];
+  let PAUSE = false;
+  let CLEAR_COLOR = [0.9, 0.6, 0.0, 1.0];
 
-function mouseHandler(ev) {
-	var scale = 0.001;
-	var dx = ev.clientX - previousLoc.x;
-	var dy = ev.clientY - previousLoc.y;
-	dx *= scale;
-	dy *= scale;
-	console.log(dx)
-	console.log(dy)
+  let gl;
+  let canvas;
+  let lights;
+  let program;
+  let camera;
+  let objects = [];
+  let pMatrix = mat4.create();
+  let previousLoc = {
+    x: 300,
+    y: 250
+  };
 
-	CAMERA_LOOK[0] += dx;
-	CAMERA_LOOK[1] -= dy;
+  class BufferInfo {
+    // Contains position and normal bufferinfo for each object
+    constructor(gl, data, numComponents) {
+      this.buffer = gl.createBuffer();
+      this.numComponents = numComponents;
 
-	previousLoc.x = ev.clientX;
-	previousLoc.y = ev.clientY;
-	
-	console.log(CAMERA_LOOK);
-}
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+      gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    }
+  }
 
-function Camera() {
-	this.translation = vec3.create();
-	this.look = vec3.create();
-}
+  class Drawable {
+    constructor(gl, shader, vertexBuffer, normalBuffer, translation = [0, 0, 0], rotation = [0, 0, 0], velocity = [0, 0, 0]) {
+      this.shader = shader;
+      this.vertexBufferInfo = vertexBuffer;
+      this.normalBufferInfo = normalBuffer;
+      this.attrs = this.getAttrLocations(this.shader, ['a_position', 'a_normal']);
+      this.translation = vec3.create();
+      this.rotation = vec3.create();
+      console.log(this.vertexBufferInfo)
+    }
 
-function Light(direction, power) {
-	this.direction = direction;
-	this.power = power;
-}
+    getAttrLocations(shader, attr_strings) {
+      // TODO: Look up a more robust approach for this
+      return attr_strings.map((location) => {
+        return gl.getAttribLocation(shader, location);
+      });
+    }
 
-function start() {
-	console.log('Started up WebGL')
-	canvas = $('#glCanvas')[0];
+    draw(gl, camera) {
+      // TODO: We shouldn't get this location on each draw
+      const u_mvMatrix = gl.getUniformLocation(this.shader, 'u_mvMatrix');
+      const primitiveType = gl.POINTS;
 
-	gl = initWebGL(canvas);
+      gl.useProgram(this.shader);
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	canvas.onmousemove = mouseHandler;
+      // Bind the vertex array
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBufferInfo.buffer);
+      gl.enableVertexAttribArray(this.attrs[0]);
+      gl.vertexAttribPointer(this.attrs[0], 3, gl.FLOAT, false, 0, 0);
 
-	if (!gl) {
-		return;
-	}
+      // Bind the normals array
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.normalBufferInfo.buffer);
+      gl.enableVertexAttribArray(this.attrs[1]);
+      gl.vertexAttribPointer(this.attrs[1], 3, gl.FLOAT, false, 0, 0);
 
-	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-	gl.clearColor(CLEAR_COLOR[0], CLEAR_COLOR[1], CLEAR_COLOR[2], CLEAR_COLOR[3]);
-	gl.enable(gl.DEPTH_TEST);
-	gl.depthFunc(gl.LEQUAL);
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+      // TODO: Does this go here? Could maybe go in update function...
+      let viewMatrix = camera.getCameraMatrix();
+      let modelMatrix = mat4.create();
+      let mvMatrix = mat4.create();
 
-	// Init some shaders
-	var vertexShaderSource = $('#vertex-shader')[0].text;
-	var fragmentShaderSource = $('#fragment-shader')[0].text;
-	var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-	var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-	program = createProgram(gl, vertexShader, fragmentShader);
-	gl.useProgram(program);
+      mat4.translate(modelMatrix, modelMatrix, this.translation);
+      mat4.rotateX(modelMatrix, modelMatrix, this.rotation[0]);
+      mat4.rotateY(modelMatrix, modelMatrix, this.rotation[1]);
+      mat4.rotateZ(modelMatrix, modelMatrix, this.rotation[2]);
+      mat4.mul(mvMatrix, viewMatrix, modelMatrix);
+      gl.uniformMatrix4fv(u_mvMatrix, false, mvMatrix);
 
-	// Set the perspective matrix
-	mat4.perspective(pMatrix, FOV, gl.canvas.width/gl.canvas.height, NEAR, FAR);
-	console.log("pMatrix");
-	console.log(pMatrix);
-	var pMatrixLoc = gl.getUniformLocation(program, "pMatrix");
-	gl.uniformMatrix4fv(pMatrixLoc, false, pMatrix);
+      gl.drawArrays(primitiveType, 0, this.vertexBufferInfo.numComponents);
+    }
 
-	// Setup buffers
-	vertexBuffer = gl.createBuffer(); // Create a buffer
-	normalBuffer = gl.createBuffer();
-	normalLineBuffer = gl.createBuffer();
+    update() {
+      this.position += this.velocity;
+    }
+  }
 
-	// Create the camera
-	camera = new Camera();
+  function draw() {
+    for (var object of objects) {
+      object.draw(gl, camera);
+    }
 
-	document.onmousedown = handleMouseDown;
-	// Normal Line Buffer
+    console.log("Finished drawing");
+  }
 
-	// Load data into buffers
+  class Sphere extends Drawable {
+    constructor(gl, shader, h_step, v_step) {
+      const sphere = Sphere.createGeo(h_step, v_step);
+      const vertexInfo = new BufferInfo(gl, sphere, sphere.length/3);
+      const normalInfo = new BufferInfo(gl, sphere, sphere.length/3);
+      const uniforms = ["a_position", "a_normal"];
+      super(gl, shader, vertexInfo, vertexInfo, uniforms); 
+    }
 
-	setupGeo();
-	setupLights(lights);
+    static createGeo(h_step, v_step) {
+      let theta;
+      let phi;
+      let vertices = [];
+      for (let i = 0; i < v_step; i++) {
+        phi = (2 * Math.PI) * (i/(v_step * 1.0));
+        for (let j = 0; j < h_step; j++) {
+          theta = (2 * Math.PI) * (j/(h_step * 1.0));
+          let x = Math.cos(theta) * Math.cos(phi);
+          let y = Math.sin(phi);
+          let z = Math.sin(theta) * Math.cos(phi);
+          vertices.push(x);
+          vertices.push(y);
+          vertices.push(z);
+        }
+      }
+      return vertices;
+    }
+  }
 
-	// Draw it
-	render(camera);
+  class Camera {
+    constructor(translation, look, velocity = [0,0,0.02]) {
+      this.translation = vec3.create();
+      this.lookAt = vec3.create();
+      this.up = vec3.create();
+      this.velocity = vec3.create();
+      this.cameraMatrix = mat4.create();
 
-	console.log('Finished');
-}
+      vec3.set(this.translation, translation[0], translation[1], translation[2]);
+      vec3.set(this.lookAt, look[0], look[1], look[2]);
+      vec3.set(this.up, 0, 1, 0);
+      vec3.set(this.velocity, velocity[0], velocity[1], velocity[2]);
+    }
 
-function handleMouseDown(event) {
-	PAUSE = !PAUSE;
-}
+    getCameraMatrix() {
+      // TODO: Optimization: if we didn't update last tick, then just return the prev matrix 
+      let x_dir = vec3.create();
+      let y_dir = vec3.create();
+      let z_dir = vec3.create();
 
-function getNormal(vertices) {
-	var v1 = vec3.create();
-	var v2 = vec3.create();
-	var cross = vec3.create();
+      vec3.sub(z_dir, this.translation, this.lookAt); // Opposite of what you would imagine
+      vec3.cross(x_dir, this.up, z_dir);
+      vec3.cross(y_dir, z_dir, x_dir);
 
-	vec3.set(v1, vertices[3] - vertices[0], vertices[4] - vertices[1], vertices[5] - vertices[2]);
-	vec3.set(v2, vertices[6] - vertices[3], vertices[7] - vertices[4], vertices[8] - vertices[5]);
-	vec3.normalize(cross, vec3.cross(cross, v2, v1));
-	return [cross[0], cross[1], cross[2]];
-}
+      vec3.normalize(x_dir, x_dir);
+      vec3.normalize(y_dir, y_dir);
+      vec3.normalize(z_dir, z_dir);
 
-function getNormalLine(shape, normal) {
-	var p1 = vec3.create();
-	var p2 = vec3.create();
-	var p3 = vec3.create();
-	var midpt = vec3.create();
-	var lineEnd = vec3.create();
+      mat4.set(this.cameraMatrix,
+       x_dir[0], x_dir[1], x_dir[2], 0,
+       y_dir[0], y_dir[1], y_dir[2], 0,
+       z_dir[0], z_dir[1], z_dir[2], 0,
+       this.translation[0], this.translation[1], this.translation[2], 1);
+      // In Progress: Is this
+      mat4.invert(this.cameraMatrix, this.cameraMatrix);
+      return this.cameraMatrix;
+    }
 
-	vec3.set(p1, shape[0], shape[1], shape[2]);
-	vec3.set(p2, shape[3], shape[4], shape[5]);
-	vec3.set(p3, shape[6], shape[7], shape[8]);
-	
-	vec3.add(midpt, p1, p2);
-	vec3.add(midpt, midpt, p3);
-	vec3.scale(midpt, midpt, 1/3.0);
+    update() {
+      vec3.add(this.translation, this.translation, this.velocity);
+    }
+  }
 
-	vec3.add(lineEnd, midpt, normal);
-	return [midpt[0], midpt[1], midpt[2], lineEnd[0], lineEnd[1], lineEnd[2]];
-}
+  function update() {
+    camera.update();
+    for (var object of objects) {
+      object.update();
+    }
+  }
 
-function randrange(min, max) {
-	return min + (Math.random() * (max - min)); 
-}
+  function setupGeo(gl, shader) {
+    for (let i = 0; i < NUM_SHAPES; i++) {
+      objects.push(new Sphere(gl, program, 40, 40));
+    }
+  }
 
-function setupGeo() {
-	positions = [];
-	normals = [];
-	// normalLines = [];
+  function start() {
+    // TODO: Remove extraneous stuff from this function
+    console.log('Started up WebGL');
+    canvas = $('#glCanvas')[0];
 
-	for (var i = 0; i < NUM_TRIANGLES; i++) {
-		var shape = createSphere(40, 40);
-		positions = positions.concat(shape);
-		for (var j = 0; j < shape.length; j += 3) {
-			normals.push(positions[j])
-			normals.push(positions[j+1])
-			normals.push((positions[j+2]))
-		}
-	}
+    gl = initWebGL(canvas);
 
-	// Position Buffer
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer); 
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    // canvas.onmousemove = mouseHandler;
 
-	// Normal Buffer
-	gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer); 
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
-}
+    if (!gl) {
+      return;
+    }
 
-function setupLights(lights) {
-	lights = []
-	lights.push(new Light([0,0,0], 0));
-}
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(CLEAR_COLOR[0], CLEAR_COLOR[1], CLEAR_COLOR[2], CLEAR_COLOR[3]);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-function createRect() {
-	var positions = [];
-	var coords = 3;
-	var numPoints = 3;
-	for (var i = 0; i < numPoints; i++) {
-		positions.push(randrange(-1, 1));
-		positions.push(randrange(-1, 1));
-		positions.push(randrange(-1, 1));
-	}
-	return positions;
-}
-
-function createSphere(h_step, v_step) {
-	var theta;
-	var phi;
-	var vertices = []
-	for (var i = 0; i < v_step; i++) {
-		phi = (2 * Math.PI) * (i/(v_step * 1.0));
-		for (var j = 0; j < h_step; j++) {
-			theta = (2 * Math.PI) * (j/(h_step * 1.0));
-			var x = Math.cos(theta) * Math.cos(phi);
-			var y = Math.sin(phi);
-			var z = Math.sin(theta) * Math.cos(phi)
-			vertices.push(x);
-			vertices.push(y);
-			vertices.push(z);
-		}
-	}
-	return vertices;
-}
-
-function setCameraMatrix() {
-	var cam_x = vec3.create();
-	var cam_y = vec3.create();
-	var cam_z = vec3.create();
-	var cam_up = vec3.create();
-	var cam_t = vec3.create();
-
-	vec3.set(cam_up, 0, 1, 0);
-	vec3.set(cam_z, CAMERA_LOOK[0], CAMERA_LOOK[1], CAMERA_LOOK[2]);
-	vec3.set(cam_t, CAMERA_TRANSLATION[0], CAMERA_TRANSLATION[1], CAMERA_TRANSLATION[2]);
-	vec3.sub(cam_z, cam_t, cam_z); // Opposite of what you would imagine
-	vec3.cross(cam_x, cam_up, cam_z);
-	vec3.cross(cam_y, cam_z, cam_x);
-
-	vec3.normalize(cam_x, cam_x);
-	vec3.normalize(cam_y, cam_y);
-	vec3.normalize(cam_z, cam_z);
-
-	mat4.set(cameraMatrix,
-	 cam_x[0], cam_x[1], cam_x[2], 0,
-	 cam_y[0], cam_y[1], cam_y[2], 0,
-	 cam_z[0], cam_z[1], cam_z[2], 0,
-	 cam_t[0], cam_t[1], cam_t[2], 1);
-
-	mat4.invert(cameraMatrix, cameraMatrix);
-}
-
-function draw() {
+    // Init some shaders
+    const vertexShaderSource = $('#vertex-shader')[0].text;
+    const fragmentShaderSource = $('#fragment-shader')[0].text;
+    const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+    program = createProgram(gl, vertexShader, fragmentShader);
     gl.useProgram(program);
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	var primitiveType = gl.POINTS;
-	var offset = 0;
-	
-	// Set camera rotation and translation
-	setCameraMatrix();
+    // Set the perspective matrix
+    mat4.perspective(pMatrix, FOV, gl.canvas.width/gl.canvas.height, NEAR, FAR);
+    const pMatrixLoc = gl.getUniformLocation(program, "u_pMatrix");
+    gl.uniformMatrix4fv(pMatrixLoc, false, pMatrix);
 
-    // Move the object!
-    mat4.identity(mvMatrix);
-	mat4.mul(mvMatrix, mvMatrix, cameraMatrix);
-    mat4.translate(mvMatrix, mvMatrix, OBJ_TRANSLATION);
-	mat4.rotateY(mvMatrix, mvMatrix, ROTATION_SPEED);
-	// Multiply the camera matrix on
+    // Create the camera
+    camera = new Camera(CAMERA_TRANSLATION, CAMERA_LOOK);
 
-	// Set uniforms
-	var uMVLocation = gl.getUniformLocation(program, "uMVMatrix");
-	gl.uniformMatrix4fv(uMVLocation, false, mvMatrix);
+    document.onmousedown = handleMouseDown;
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer); 
-	var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-	gl.enableVertexAttribArray(positionAttributeLocation);
-	gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+    setupGeo(gl);
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer); 
-	var normalAttributeLocation = gl.getAttribLocation(program, "a_normal");
-	gl.enableVertexAttribArray(normalAttributeLocation);
-	gl.vertexAttribPointer(normalAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+    // Draw it
+    render();
 
-	// gl.drawArrays(primitiveType, offset, 3 * NUM_TRIANGLES);
-	gl.drawArrays(primitiveType, offset, positions.length / 3);
+    console.log('Finished');
+  }
 
-	if (DRAW_NORMALS) {
-		// drawNormals();
-	}
+  function handleMouseDown(event) {
+    PAUSE = !PAUSE;
+  }
 
-	console.log("Finished drawing");
+  function randrange(min, max) {
+    return min + (Math.random() * (max - min)); 
+  }
+
+  function render() {
+    requestAnimFrame(render);
+    if (!PAUSE) {
+      update();
+      draw();
+    }
+  }
+
+  function initWebGL(canvas) {
+    gl = null;
+    gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+    if (!gl) {
+      alert('No WebGL support found.');
+    }
+    return gl;
+  }
+
+  function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+      console.log('Compiled shader program');
+      return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+
+  }
+
+  function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+      console.log('Compiled shader ' + type);
+      return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+  }
+
+  function mouseHandler(ev) {
+    var scale = 0.001;
+    var dx = ev.clientX - previousLoc.x;
+    var dy = ev.clientY - previousLoc.y;
+    dx *= scale;
+    dy *= scale;
+
+    CAMERA_LOOK[0] += dx;
+    CAMERA_LOOK[1] -= dy;
+
+    previousLoc.x = ev.clientX;
+    previousLoc.y = ev.clientY;
+    
+    console.log(CAMERA_LOOK);
 }
-
-function render() {
-	requestAnimFrame(render);
-	if (!PAUSE) {
-		draw();
-	}
-}
-
-function initWebGL(canvas) {
-	gl = null;
-	gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-	if (!gl) {
-		alert('No WebGL support found.')
-	}
-	return gl;
-}
-
-function createProgram(gl, vertexShader, fragmentShader) {
-	var program = gl.createProgram();
-	gl.attachShader(program, vertexShader);
-	gl.attachShader(program, fragmentShader);
-	gl.linkProgram(program);
-
-	var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-	if (success) {
-		console.log('Compiled shader program');
-		return program;
-	}
-
-	console.log(gl.getProgramInfoLog(program));
-	gl.deleteProgram(program);
-
-}
-
-function createShader(gl, type, source) {
-	var shader = gl.createShader(type);
-	gl.shaderSource(shader, source);
-	gl.compileShader(shader);
-
-	var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-	if (success) {
-		console.log('Compiled shader ' + type);
-		return shader;
-	}
-
-	console.log(gl.getShaderInfoLog(shader));
-	gl.deleteShader(shader);
-}
-
-// Handlers
-
